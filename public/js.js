701baeab91bf3288d29392938f210d5c419c9de0
le hai <hai.lethanh1295@gmail.com>
2017-11-07 21:14:39 +0700

    update day 11

--- public/js.js
+++ public/js.js
@@ -4,13 +4,27 @@
 var app = angular.module('myApp', []);
 
 app.controller('myCtrl', function($scope) {
+    /* data list */
     $scope.info = [
         {id:1, name: "anh", email: "abc123@xyz", contact: "0123456789", position: "abc street"},
         {id:2, name: "anh", email: "abc123@xyz", contact: "0123456789", position: "abc street"},
         {id:3, name: "anh", email: "abc123@xyz", contact: "0123456789", position: "abc street"},
     ];
+    /* detail form */
       $scope.showData = function(id) {
         $scope.data={ name: "anh", email: "abc123@xyz", contact: "0123456789", position: "abc street"};
         $("#showData1").modal("show");
-      }
+      };
+      /* edit button */
+      $scope.showEdit = function(id) {
+        $scope.edit = { name: "anh", email: "abc123@xyz", contact: "0123456789", position: "abc street"};
+   $("#showEdit1").modal("show");  
+  };
+  /* confirm pop up */
+   $scope.showConfirm = function(id) {
+   $("#deleteConfirm").modal("show");  
+  };
+  /* Delete after confirm */
+  
 });
+
