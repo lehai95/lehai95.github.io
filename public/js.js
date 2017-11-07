77649e421938777f4251492583025e127024cb23
le hai <hai.lethanh1295@gmail.com>
2017-11-07 18:52:30 +0700

    2

--- public/js.js
+++ public/js.js
@@ -1,11 +1,16 @@
-var app = angular.module('EmployeeManagement', []);
-app.controller('ListEmployeeController', function($scope) {
-    $scope.employeeList = 
-    [
-        {id: 123, name: "anh",...},
-        {id: 123, name: "anh",...},
-        {id: 123, name: "anh",...}
+
+
+
+var app = angular.module('myApp', []);
+
+app.controller('myCtrl', function($scope) {
+    $scope.info = [
+        {id:1, name: "anh", email: "abc123@xyz", contact: "0123456789", position: "abc street"},
+        {id:2, name: "anh", email: "abc123@xyz", contact: "0123456789", position: "abc street"},
+        {id:3, name: "anh", email: "abc123@xyz", contact: "0123456789", position: "abc street"},
     ];
-}
-    id:123
-    name:
+      $scope.showData = function(id) {
+        $scope.data={ name: "anh", email: "abc123@xyz", contact: "0123456789", position: "abc street"};
+        $("#showData1").modal("show");
+      }
+});
