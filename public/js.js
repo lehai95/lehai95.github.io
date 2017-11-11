4256a5ee89028e15f7b46628368883c10da7f83d
le hai <hai.lethanh1295@gmail.com>
2017-11-11 20:52:25 +0700

    day13

--- public/js.js
+++ public/js.js
@@ -3,25 +3,41 @@
 
 var app = angular.module('myApp', []);
 
-app.controller('myCtrl', function($scope) {
+app.controller('myCtrl', function($scope, $http) {
     /* data list */
-    $scope.info = [
+    /*$scope.info = [
         {id:1, name: "anh", email: "abc123@xyz", contact: "0123456789", position: "abc street"},
         {id:2, name: "anh", email: "abc123@xyz", contact: "0123456789", position: "abc street"},
         {id:3, name: "anh", email: "abc123@xyz", contact: "0123456789", position: "abc street"},
         {id:4, name: "anh", email: "abc123@xyz", contact: "0123456789", position: "abc street"},
         {id:5, name: "anh", email: "abc123@xyz", contact: "0123456789", position: "abc street"},
-    ];
+    ];*/
+    $http({
+method: "GET",
+url: 'http://192.168.1.30:8080/angulara/public/api/v1/employees?page=1'
+    }).then(function successCallback(response){
+$scope.employeesList = response.data;
+
+});
+
     /* add info */
      $scope.showAdd = function(id) {
        $scope.data1 = { name: "type your name", email: "type your email", contact: "type your contact number", position: "type your position"};
         $("#addNew").modal("show");
       };
     /* detail form */
-      $scope.showData = function(id) {
-        $scope.data={ name: "anh", email: "abc123@xyz", contact: "0123456789", position: "abc street"};
+     /*$scope.showData = function(id) {
+        $scope.data2={ name: "anh", email: "abc123@xyz", contact: "0123456789", position: "abc street"};
         $("#showData1").modal("show");
-      };
+      }; */
+      /*$http({
+method: "POST",
+url: 'http://192.168.56.1:8080/angulara/public/api/v1/employees?page=1'
+data: 'name' : $scope.name; 'email' : $scope.email; 'contact' : $scope.contact; 'position' : $scope.postion;
+    }).then(function successCallback(response){
+$scope.data2 = response.data;
+
+});
       /* edit button */
       $scope.showEdit = function(id) {
         $scope.edit = { name: "anh", email: "abc123@xyz", contact: "0123456789", position: "abc street"};
